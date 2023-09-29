a6baf7501245f885eb2e5e53421c7b3325ccaadb
simo <wambuisimon712@gmail.com>
2023-09-29 17:08:28 +0300

    add: new face

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import "./App.css";
+import {
+  BsEmojiSmileFill,
+  BsFillEmojiHeartEyesFill,
+  BsFillHeartFill,
+} from "react-icons/bs";
 
 function App() {
   return (
     <>
       <div>
-        <h1>Hello world!</h1>
+        <h1>
+          Hello Friend!
+          <BsEmojiSmileFill className="smile" />
+          Thanks for you time to check out on my work. Soon I will updating
+          them.
+          <BsFillEmojiHeartEyesFill className="heart-eye" />
+        </h1>
+
+        <h2>
+          I hope You have an amazing time and be blessed.
+          <BsFillHeartFill className="heart" />
+        </h2>
       </div>
     </>
   );
 }
 
 export default App;
+
+// BsFillEmojiHeartEyesFill
+// BsFillEmojiKissFill;
+// BsFillHeartFill;
+// BsEmojiSmileFill;
